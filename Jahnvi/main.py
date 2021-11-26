828498e9da6ca947162492a2df8f70fe10de3965
Rexxxxxo010201 <69809448+Rexxxxxo010201@users.noreply.github.com>
2021-11-26 07:49:24 +0530

    Update main.py

--- Jahnvi/main.py
+++ Jahnvi/main.py
@@ -5,8 +5,6 @@ from strategies.run_zipline import run_strategy
 
 
 def main():
-    print("*** PackPub - Hands-on Machine Learning for Algorithmic Trading Bots ***")
-    print("*** SEC005/VID002: Implement Scalping Strategy ***")
     perf = run_strategy("scalping")
     perf.to_csv("scalping.csv")
 
